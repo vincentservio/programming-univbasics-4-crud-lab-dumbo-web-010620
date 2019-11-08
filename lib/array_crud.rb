9b985f316008c70fc45ea5af0663c2736d980b29
vincent servio <vincentservio@gmail.com>
2019-11-08 23:03:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -45,7 +45,7 @@ end
 def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[3] = "really"
-  => "totally"
+  array.=> "totally"
 
 
 end
